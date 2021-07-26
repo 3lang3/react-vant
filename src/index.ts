0b0dca81444080ca762c17c1a1e5630985bb5059
浪浪浪 <675483520@qq.com>
2021-07-26 13:30:12 +0800

    feat: add action-sheet

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ export { default as PullRefresh } from './pull-refresh';
 export { default as NoticeBar } from './notice-bar';
 export { default as List } from './list';
 export { default as Toast } from './toast';
+export { default as ActionSheet } from './action-sheet';
 export { default as Dialog } from './dialog';
 export { default as Search } from './search';
 export { default as Swipe } from './swipe';
--- vant.config.js
+++ vant.config.js
@@ -154,6 +154,10 @@ module.exports = {
             title: '反馈组件',
             items: [
               {
+                path: 'action-sheet',
+                title: 'ActionSheet 动作面板',
+              },
+              {
                 path: 'dialog',
                 title: 'Dialog 弹出框',
               },
