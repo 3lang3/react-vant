e6a4bb04c1a7c544dd89f8c03cae5b3954466845
3lang <675483520@qq.com>
2021-09-01 11:38:22 +0800

    docs: collapse zh note

--- src/collapse/PropsType.ts
+++ src/collapse/PropsType.ts
@@ -1,19 +1,26 @@
 import { BaseTypeProps } from '../utils';
-
 import { CellProps } from '../cell/PropsType';
 
 export interface CollapseProps extends BaseTypeProps {
+  /** 是否开启手风琴模式	 */
   accordion: boolean;
+  /** 默认打开的面板name */
   initExpanded: string | number | Array<number | string>;
+  /** 是否显示外边框	 */
   border: boolean;
+  /** 当前展开面板的 name	 */
   value?: number | string | Array<number | string>;
-  onChange: (name: number | string | Array<number | string>) => void;
+  /** 切换面板时触发	*/
+  onChange: (activeNames: number | string | Array<number | string>) => void;
 }
 
 export interface CollapseItemProps extends CellProps {
+  /** 唯一标识符，默认为索引值	 */
   name?: number | string;
+  /** 是否禁用面板	 */
   disabled?: boolean;
-  isLink?: boolean;
-  index?: number;
+  /** 是否为只读状态，只读状态下无法操作面板	 */
   readonly?: boolean;
+  /** 内部使用 */
+  index?: number;
 }
