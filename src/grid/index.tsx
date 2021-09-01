416a71995496cef0547bf0104ab8280126566a0a
3lang <675483520@qq.com>
2021-09-01 17:15:52 +0800

    fix: grid sync & docs correct

--- src/grid/index.tsx
+++ src/grid/index.tsx
@@ -4,3 +4,5 @@ import GridItem from './GridItem';
 const GridNamespace = Object.assign(Grid, { Item: GridItem });
 
 export default GridNamespace;
+export { Grid, GridItem };
+export type { GridProps, GridItemProps, GridDirection } from './PropsType';
