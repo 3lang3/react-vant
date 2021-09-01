2c0649d36272d2a47326e5047151b3b890f10e7f
3lang <675483520@qq.com>
2021-09-01 11:43:13 +0800

    fix: countdown sycn & docs correct

---
+++ src/count-down/CountDown.tsx
@@ -0,0 +1,57 @@
+import React, { forwardRef, useEffect, useImperativeHandle, useMemo } from 'react';
+import cls from 'classnames';
+import { CountDownProps, CountDownInstance } from './PropsType';
+import { createNamespace, noop } from '../utils';
+import { useCountDown } from './useCountDown';
+import { parseFormat } from './utils';
+
+const [bem] = createNamespace('count-down');
+
+const CountDown = forwardRef<CountDownInstance, CountDownProps>((props, ref) => {
+  const { start, pause, reset, current } = useCountDown({
+    time: +props.time,
+    millisecond: props.millisecond,
+    onChange: props.onChange,
+    onFinish: props.onFinish,
+  });
+
+  const timeText = useMemo(() => parseFormat(props.format, current), [current]);
+
+  const resetTime = () => {
+    reset(+props.time);
+
+    if (props.autoStart) {
+      start();
+    }
+  };
+
+  useEffect(() => {
+    resetTime();
+
+    return () => {
+      pause();
+    };
+  }, [props.time]);
+
+  useImperativeHandle(ref, () => ({
+    start,
+    pause,
+    reset: resetTime,
+  }));
+
+  return (
+    <div className={cls(props.className, bem())} style={props.style}>
+      {props.children ? props.children(current) : timeText}
+    </div>
+  );
+});
+
+CountDown.defaultProps = {
+  autoStart: true,
+  time: 0,
+  format: 'HH:mm:ss',
+  onChange: noop,
+  onFinish: noop,
+};
+
+export default CountDown;
--- src/count-down/PropsType.ts
+++ src/count-down/PropsType.ts
@@ -1,11 +1,17 @@
 import { BaseTypeProps } from '../utils';
 
 export type CurrentTime = {
-  days: number;
-  hours: number;
+  /** 剩余总时间（单位毫秒）	 */
   total: number;
+  /** 剩余天数	 */
+  days: number;
+  /** 剩余小时	 */
+  hours: number;
+  /** 剩余分钟	 */
   minutes: number;
+  /** 剩余秒数	 */
   seconds: number;
+  /** 剩余毫秒	 */
   milliseconds: number;
 };
 
@@ -17,17 +23,37 @@ export type UseCountDownOptions = {
 };
 
 export type CountDownInstance = {
+  /** 开始倒计时	 */
   start: () => void;
+  /** 暂停倒计时	 */
   pause: () => void;
+  /** 重设倒计时，若 autoStart 为 true，重设后会自动开始倒计时	 */
   reset: () => void;
 };
 
 export interface CountDownProps extends BaseTypeProps {
+  /**
+   * 是否自动开始倒计时
+   * @default true
+   */
   autoStart?: boolean;
+  /**
+   * 是否开启毫秒级渲染
+   */
   millisecond?: boolean;
+  /**
+   * 倒计时时长，单位毫秒
+   * @default 0
+   */
   time?: number | string;
+  /**
+   * 时间格式
+   * @default 'HH:mm:ss'
+   */
   format?: string;
+  /** 倒计时变化时触发	 */
   onChange?: (currentTime: CurrentTime) => void;
+  /** 倒计时结束时触发	*/
   onFinish?: () => void;
   children?: (currentTime: CurrentTime) => React.ReactNode;
 }
