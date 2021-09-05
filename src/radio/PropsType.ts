8d88f4c9a1c2f52f9613c740c7405c70e1278de1
3lang <675483520@qq.com>
2021-09-05 21:59:05 +0800

    docs: radio type notes

--- src/radio/PropsType.ts
+++ src/radio/PropsType.ts
@@ -4,13 +4,21 @@ import { CheckerProps } from '../checkbox/PropsType';
 type RadioValueType = number | string;
 
 export interface RadioGroupProps<T = RadioValueType> extends BaseTypeProps {
+  /** 是否为禁用状态	 */
   disabled?: boolean;
+  /** 图标大小，默认单位为px	 */
   iconSize?: number | string;
+  /** 排列方向，可选值为horizontal	 */
   direction?: string;
+  /** 选中状态颜色	 */
   checkedColor?: string;
+  /** 当前值 */
   value?: T;
+  /** 默认值 */
   defaultValue?: T;
+  /** 是否开启异步更新 */
   asyncChange?: boolean;
+  /** 当绑定值变化时触发的事件	 */
   onChange?: (name: T) => void;
 }
 
