6bd680ec4f7e0a38dab630abe5a26b1c0926b958
3lang <675483520@qq.com>
2021-09-09 19:50:35 +0800

    fix: #57

--- src/sidebar/PropsType.ts
+++ src/sidebar/PropsType.ts
@@ -12,16 +12,24 @@ export type SidebarProvide = {
 };
 
 export interface SidebarProps extends BaseTypeProps {
-  value?: number | string;
-  defaultValue?: number | string;
-  onChange?: (value: number | string) => void;
+  /** 当前导航项的索引	 */
+  value?: number;
+  /** 默认索引 */
+  defaultValue?: number;
+  /** 切换导航项时触发	 */
+  onChange?: (value: number) => void;
   children?: React.ReactNode;
 }
 
 export interface SidebarItemProps {
+  /** 是否显示右上角小红点	 */
   dot?: boolean;
+  /** 内容 */
   title?: React.ReactNode;
+  /** 图标右上角徽标的内容	 */
   badge?: number | string;
+  /** v */
   disabled?: boolean;
+  /** 点击时触发	 */
   onClick?: (value: number) => void;
 }
