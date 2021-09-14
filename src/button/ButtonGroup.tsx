54c0fd1081098dfe4fb456f6b6bbdd2474d09406
潇月 <565870260@qq.com>
2021-09-14 16:05:59 +0800

    feat: add button group

--- src/button/ButtonGroup.tsx
+++ src/button/ButtonGroup.tsx
@@ -3,8 +3,9 @@ import classnames from 'classnames';
 import { BaseTypeProps, createNamespace } from '../utils';
 
 const [bem] = createNamespace('button-group');
-export const ButtonGroup: FC<BaseTypeProps> = ({ className, children }) => (
-  <div className={classnames(className, bem())}>{children}</div>
+
+export const ButtonGroup: FC<BaseTypeProps> = ({ className,style, children }) => (
+  <div style={style} className={classnames(className, bem())}>{children}</div>
 );
 
 export default ButtonGroup;
