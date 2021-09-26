0a50511a28d5cd50fb4d006055ffa2894720f091
3lang <675483520@qq.com>
2021-09-26 17:29:46 +0800

    feat: uploader use css var

--- packages/react-vant/src/uploader/index.tsx
+++ packages/react-vant/src/uploader/index.tsx
@@ -2,4 +2,4 @@ import Uploader from './Uploader';
 
 export default Uploader;
 export { Uploader };
-export type { UploaderProps, UploaderInstance, UploaderFileListItem } from './PropsType';
+export type { UploaderProps, UploaderResultType, UploaderInstance, UploaderFileListItem } from './PropsType';
