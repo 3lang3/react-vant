ed7849b1da8ab64eaac681050f3a08e6d8df0f08
3lang <675483520@qq.com>
2021-09-29 09:13:11 +0800

    fix: button group css var

--- packages/react-vant/src/button/ButtonGroup.tsx
+++ packages/react-vant/src/button/ButtonGroup.tsx
@@ -5,7 +5,7 @@ import ConfigProviderContext from '../config-provider/ConfigProviderContext';
 
 export const ButtonGroup: FC<BaseTypeProps> = ({ className, style, children }) => {
   const { prefixCls, createNamespace } = useContext(ConfigProviderContext);
-  const [bem] = createNamespace('button', prefixCls);
+  const [bem] = createNamespace('button-group', prefixCls);
 
   return (
     <div style={style} className={classnames(className, bem())}>
