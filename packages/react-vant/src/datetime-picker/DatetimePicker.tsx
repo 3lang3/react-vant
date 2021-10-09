6c353b864b0002613e4ff61ccade54f86cfd32a8
3lang <675483520@qq.com>
2021-10-09 11:35:10 +0800

    fix: datetimepicker - correct type value - timepicker value correct

--- packages/react-vant/src/datetime-picker/DatetimePicker.tsx
+++ packages/react-vant/src/datetime-picker/DatetimePicker.tsx
@@ -4,17 +4,23 @@ import classnames from 'classnames';
 import DatePicker from './DatePicker';
 import TimePicker from './TimePicker';
 
-import { DateTimePickerProps, DateTimePickerInstance } from './PropsType';
+import {
+  DateTimePickerProps,
+  DateTimePickerInstance,
+  TimePickerProps,
+  DatePickerProps,
+} from './PropsType';
 import ConfigProviderContext from '../config-provider/ConfigProviderContext';
 
 const DateTimePicker = forwardRef<DateTimePickerInstance, DateTimePickerProps>((props, ref) => {
-  const { prefixCls,  createNamespace } = useContext(ConfigProviderContext);
+  const { prefixCls, createNamespace } = useContext(ConfigProviderContext);
   const [bem] = createNamespace('datetime-picker', prefixCls);
 
   const isTimePicker = props.type === 'time';
-  const Component = isTimePicker ? TimePicker : DatePicker;
 
-  return <Component ref={ref} className={classnames(bem())} {...props} />;
+  if (isTimePicker)
+    return <TimePicker ref={ref} className={classnames(bem())} {...(props as TimePickerProps)} />;
+  return <DatePicker ref={ref} className={classnames(bem())} {...(props as DatePickerProps)} />;
 });
 
 export default DateTimePicker;
