9eb4547b0d5dab10f5ed8b9076a456c3977447fd
TinsFox <1414849373@qq.com>
2021-11-02 14:40:24 +0800

    docs(Sidebar): update doc

--- packages/react-vant/src/sidebar/demo/index.tsx
+++ packages/react-vant/src/sidebar/demo/index.tsx
@@ -9,43 +9,43 @@ export default (): React.ReactNode => {
   const [ac, setAc] = useState(2);
   return (
     <DemoSection className="demo-sidebar">
-      {/* <DemoBlock title="基础用法"> */}
-      {/*  <Sidebar> */}
-      {/*    <Sidebar.Item title="标签名" /> */}
-      {/*    <Sidebar.Item title="标签名" /> */}
-      {/*    <Sidebar.Item title="标签名" /> */}
-      {/*  </Sidebar> */}
-      {/* </DemoBlock> */}
+      <DemoBlock title="基础用法">
+        <Sidebar>
+          <Sidebar.Item title="标签名" />
+          <Sidebar.Item title="标签名" />
+          <Sidebar.Item title="标签名" />
+        </Sidebar>
+      </DemoBlock>
 
-      {/* <DemoBlock title="徽标提示"> */}
-      {/*  <Sidebar> */}
-      {/*    <Sidebar.Item title="标签名" dot /> */}
-      {/*    <Sidebar.Item title="标签名" badge={5} /> */}
-      {/*    <Sidebar.Item title="标签名" badge={20} /> */}
-      {/*  </Sidebar> */}
-      {/* </DemoBlock> */}
+      <DemoBlock title="徽标提示">
+        <Sidebar>
+          <Sidebar.Item title="标签名" dot />
+          <Sidebar.Item title="标签名" badge={5} />
+          <Sidebar.Item title="标签名" badge={20} />
+        </Sidebar>
+      </DemoBlock>
 
-      {/* <DemoBlock title="禁用选项"> */}
-      {/*  <Sidebar> */}
-      {/*    <Sidebar.Item title="标签名" /> */}
-      {/*    <Sidebar.Item title="标签名" disabled /> */}
-      {/*    <Sidebar.Item title="标签名" /> */}
-      {/*  </Sidebar> */}
-      {/* </DemoBlock> */}
+      <DemoBlock title="禁用选项">
+        <Sidebar>
+          <Sidebar.Item title="标签名" />
+          <Sidebar.Item title="标签名" disabled />
+          <Sidebar.Item title="标签名" />
+        </Sidebar>
+      </DemoBlock>
 
-      {/* <DemoBlock title="监听切换事件"> */}
-      {/*  <Sidebar */}
-      {/*    value={ac} */}
-      {/*    onChange={(v) => { */}
-      {/*      setAc(v); */}
-      {/*      Toast.info(`标签名 ${v + 1}`); */}
-      {/*    }} */}
-      {/*  > */}
-      {/*    <Sidebar.Item title="标签名1" /> */}
-      {/*    <Sidebar.Item title="标签名2" /> */}
-      {/*    <Sidebar.Item title="标签名3" /> */}
-      {/*  </Sidebar> */}
-      {/* </DemoBlock> */}
+      <DemoBlock title="监听切换事件">
+        <Sidebar
+          value={ac}
+          onChange={(v) => {
+            setAc(v);
+            Toast.info(`标签名 ${v + 1}`);
+          }}
+        >
+          <Sidebar.Item title="标签名1" />
+          <Sidebar.Item title="标签名2" />
+          <Sidebar.Item title="标签名3" />
+        </Sidebar>
+      </DemoBlock>
       <DemoBlock title="自定义内容区">
         <Sidebar
           value={ac}
