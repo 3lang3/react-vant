91d1f36dfeed59cf650994215024072e4d83fa9d
3lang <675483520@qq.com>
2021-11-09 11:33:18 +0800

    fix: useRefState 移除useLayoutEffect兼容ssr #144

--- packages/react-vant/src/hooks/use-ref-state.ts
+++ packages/react-vant/src/hooks/use-ref-state.ts
@@ -1,10 +1,10 @@
-import { useLayoutEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function useRefState<T>(initialState: T | (() => T)) {
   const [state, setState] = useState<T>(initialState);
   const ref = useRef<T>(state);
-  useLayoutEffect(() => {
+  useEffect(() => {
     ref.current = state;
   }, [state]);
   return [state, setState, ref] as const;
