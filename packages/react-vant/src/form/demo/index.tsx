16a091bdf7a4c84f6a2762a7f047f433716029c3
Jungzl <35426360+Jungzl@users.noreply.github.com>
2022-01-25 11:40:17 +0800

    fix: remove form default cell.group children (#309) (#312)

--- packages/react-vant/src/form/demo/index.tsx
+++ packages/react-vant/src/form/demo/index.tsx
@@ -1,20 +1,21 @@
 /* eslint-disable no-console */
 import React, { useState } from 'react';
 import {
+  Button,
+  Calendar,
+  Cell,
+  Checkbox,
+  DatetimePicker,
   Field,
+  Picker,
+  Popup,
+  Radio,
   Rate,
   Slider,
   Stepper,
-  Checkbox,
-  Radio,
   Switch,
   Toast,
-  Button,
-  Picker,
-  Popup,
   Uploader,
-  DatetimePicker,
-  Calendar,
 } from 'react-vant';
 import { AddO, Delete } from '@react-vant/icons';
 import { components } from 'site-mobile-demo';
@@ -129,7 +130,6 @@ export default (): React.ReactNode => {
 
       <DemoBlock title="表单类型">
         <Form
-          inset
           form={form}
           onFinish={onFinish}
           footer={
@@ -210,45 +210,52 @@ export default (): React.ReactNode => {
             </div>
           }
         >
-          <Form.List name="users" initialValue={[{ name: 'react-vant', age: '1' }]}>
-            {(fields, { add, remove }) => (
-              <>
-                {fields.map((field, idx) => (
-                  <div className="form-list-item" key={field.key}>
-                    <h6>用户{idx + 1}:</h6>
-                    <div className="form-list-item__control">
-                      <Form.Item
-                        label="姓名"
-                        name={[field.name, 'name']}
-                        rules={[
-                          { type: 'string', min: 2, max: 6, message: '姓名最少两个字，最多6个字' },
-                        ]}
-                      >
-                        <Field placeholder="请输入用户姓名" />
-                      </Form.Item>
-                      <Form.Item
-                        label="年龄"
-                        name={[field.name, 'age']}
-                        rules={[
-                          { type: 'number', message: '请输入数字', transform: (v) => Number(v) },
-                        ]}
-                      >
-                        <Field
-                          placeholder="请输入用户年龄"
-                          rightIcon={<Delete onClick={() => remove(idx)} />}
-                        />
-                      </Form.Item>
+          <Cell.Group inset>
+            <Form.List name="users" initialValue={[{ name: 'react-vant', age: '1' }]}>
+              {(fields, { add, remove }) => (
+                <>
+                  {fields.map((field, idx) => (
+                    <div className="form-list-item" key={field.key}>
+                      <h6>用户{idx + 1}:</h6>
+                      <div className="form-list-item__control">
+                        <Form.Item
+                          label="姓名"
+                          name={[field.name, 'name']}
+                          rules={[
+                            {
+                              type: 'string',
+                              min: 2,
+                              max: 6,
+                              message: '姓名最少两个字，最多6个字',
+                            },
+                          ]}
+                        >
+                          <Field placeholder="请输入用户姓名" />
+                        </Form.Item>
+                        <Form.Item
+                          label="年龄"
+                          name={[field.name, 'age']}
+                          rules={[
+                            { type: 'number', message: '请输入数字', transform: (v) => Number(v) },
+                          ]}
+                        >
+                          <Field
+                            placeholder="请输入用户年龄"
+                            rightIcon={<Delete onClick={() => remove(idx)} />}
+                          />
+                        </Form.Item>
+                      </div>
                     </div>
+                  ))}
+                  <div style={{ padding: 10 }}>
+                    <Button round block plain icon={<AddO />} size="small" onClick={() => add()}>
+                      新增用户
+                    </Button>
                   </div>
-                ))}
-                <div style={{ padding: 10 }}>
-                  <Button round block plain icon={<AddO />} size="small" onClick={() => add()}>
-                    新增用户
-                  </Button>
-                </div>
-              </>
-            )}
-          </Form.List>
+                </>
+              )}
+            </Form.List>
+          </Cell.Group>
         </Form>
       </DemoBlock>
     </DemoSection>
