2698471ce883c0b48dec02a967e471320dcb71a7
3lang <675483520@qq.com>
2022-02-08 10:27:07 +0800

    docs: coupon-list readme update

---
+++ packages/react-vant/src/coupon-list/demo/demo.ts
@@ -0,0 +1,34 @@
+export const data = {
+  id: 1,
+  condition: '无使用门槛\n最多优惠12元',
+  reason: '',
+  value: 150,
+  name: '优惠券名称',
+  description: '描述信息',
+  startAt: 1489104000,
+  endAt: 1514592000,
+  valueDesc: '1.5',
+  unitDesc: '元',
+};
+
+export const discountCoupon = {
+  ...data,
+  id: 2,
+  value: 12,
+  valueDesc: '8.8',
+  unitDesc: '折',
+};
+
+export const disabledCoupon = {
+  ...data,
+  id: 3,
+  reason: '优惠券不可用原因',
+};
+
+export const disabledDiscountCoupon = {
+  ...discountCoupon,
+  valueDesc: '1',
+  unitDesc: '折',
+  id: 4,
+  reason: '优惠券不可用原因',
+};
--- packages/react-vant/src/coupon-list/demo/index.tsx
+++ packages/react-vant/src/coupon-list/demo/index.tsx
@@ -1,48 +1,10 @@
-/* eslint-disable no-console */
 import React, { useState } from 'react';
-import { components } from 'site-mobile-demo';
-import { Popup, Toast, CouponCell } from 'react-vant';
-import { CouponList } from '..';
-import './style.less';
-
-const data = {
-  id: 1,
-  condition: '无使用门槛\n最多优惠12元',
-  reason: '',
-  value: 150,
-  name: '优惠券名称',
-  description: '描述信息',
-  startAt: 1489104000,
-  endAt: 1514592000,
-  valueDesc: '1.5',
-  unitDesc: '元',
-};
-const discountCoupon = {
-  ...data,
-  id: 2,
-  value: 12,
-  valueDesc: '8.8',
-  unitDesc: '折',
-};
-
-const disabledCoupon = {
-  ...data,
-  id: 3,
-  reason: '优惠券不可用原因',
-};
-
-const disabledDiscountCoupon = {
-  ...discountCoupon,
-  valueDesc: '1',
-  unitDesc: '折',
-  id: 4,
-  reason: '优惠券不可用原因',
-};
+import { CouponList, Popup, Toast, CouponCell } from 'react-vant';
+import { data, disabledDiscountCoupon, disabledCoupon } from './demo';
 
 const getRandomId = (max = 999999) => String(Math.floor(Math.random() * max) + 1);
 
 export default () => {
-  const { DemoBlock, DemoSection } = components;
   const [coupons, setCoupons] = useState([data, disabledDiscountCoupon]);
   const [disabledCoupons] = useState([disabledCoupon, disabledDiscountCoupon]);
   const [chosenCoupon, setChosenCoupon] = useState(-1);
@@ -54,37 +16,29 @@ export default () => {
   };
 
   const onExchange = (code) => {
-    console.log('object', code);
+    console.log('code: ', code);
     Toast('兑换成功');
     setCoupons((v) => [...v, { ...data, id: getRandomId() } as any]);
   };
 
-  console.log(coupons, disabledCoupons);
-
   return (
-    <DemoSection>
-      <DemoBlock title="基础用法">
-        <CouponCell
-          coupons={coupons}
+    <>
+      <CouponCell coupons={coupons} chosenCoupon={chosenCoupon} onClick={() => setVisible(true)} />
+      <Popup
+        round
+        position="bottom"
+        style={{ height: '90%', paddingTop: 4 }}
+        visible={visible}
+        onClose={() => setVisible(false)}
+      >
+        <CouponList
           chosenCoupon={chosenCoupon}
-          onClick={() => setVisible(true)}
+          coupons={coupons}
+          disabledCoupons={disabledCoupons}
+          onChange={onChange}
+          onExchange={onExchange}
         />
-        <Popup
-          round
-          position="bottom"
-          style={{ height: '90%', paddingTop: 4 }}
-          visible={visible}
-          onClose={() => setVisible(false)}
-        >
-          <CouponList
-            chosenCoupon={chosenCoupon}
-            coupons={coupons}
-            disabledCoupons={disabledCoupons}
-            onChange={onChange}
-            onExchange={onExchange}
-          />
-        </Popup>
-      </DemoBlock>
-    </DemoSection>
+      </Popup>
+    </>
   );
 };
