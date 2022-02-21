14bd346234fcd637bbac9f6c45bd2c91eb499730
3lang <675483520@qq.com>
2022-02-21 15:37:03 +0800

    docs(Cell): update avatar list demo

--- packages/react-vant/src/cell/demo/list.tsx
+++ packages/react-vant/src/cell/demo/list.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Cell, Image } from 'react-vant';
 
-const demoAvatarImages = [
-  'https://images.unsplash.com/photo-1548532928-b34e3be62fc6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-  'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9',
-  'https://images.unsplash.com/photo-1542624937-8d1e9f53c1b9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-  'https://images.unsplash.com/photo-1546967191-fdfb13ed6b1e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-];
-
 export default () => {
   return (
     <>
-      {demoAvatarImages.map((src, idx) => (
+      {Array.from({ length: 4 }).map((_, idx) => (
         <Cell
           center
-          key={src}
+          key={idx}
           title={`Avatar ${idx}`}
           label="Deserunt dolor ea eaque eos"
-          icon={<Image width={44} height={44} src={src} round />}
+          icon={
+            <Image
+              width={44}
+              height={44}
+              src="https://cdn.jsdelivr.net/gh/3lang3/react-vant@main/public/home-music-card-1.jpg"
+              round
+            />
+          }
+          isLink
         />
       ))}
     </>
