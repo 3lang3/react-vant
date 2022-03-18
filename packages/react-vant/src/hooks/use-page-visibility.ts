a4a6a90cf47b5505c7200996a91891b492fdfcd7
3lang <675483520@qq.com>
2022-03-18 14:49:04 +0800

    fix: usePageVisibility 类型错误修复

--- packages/react-vant/src/hooks/use-page-visibility.ts
+++ packages/react-vant/src/hooks/use-page-visibility.ts
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import { inBrowser } from '../utils';
 import useEventListener from './use-event-listener';
 
+type VisibilityState = 'visible' | 'hidden';
+
 function usePageVisibility(): VisibilityState {
   const [visibility, _setVisibility] = useState<VisibilityState>('visible');
 
