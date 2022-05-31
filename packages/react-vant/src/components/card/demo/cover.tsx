d6f9403795cd2c85ef28b56bea4e0afb452e5599
3lang <675483520@qq.com>
2022-05-31 09:28:04 +0800

    docs: update demo static to host server

--- packages/react-vant/src/components/card/demo/cover.tsx
+++ packages/react-vant/src/components/card/demo/cover.tsx
@@ -7,7 +7,7 @@ export default () => {
     <>
       <Card round style={{ marginBottom: 20 }}>
         <Card.Cover onClick={() => Toast.info('点击了Cover区域')}>
-          <Image src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/baa20698931623.5ee79b6a8ec2b.jpg" />
+          <Image src="/demo_avatar_1.jpg" />
         </Card.Cover>
         <Card.Header extra={<Arrow />} onClick={() => Toast.info('点击了Header区域')}>
           封面展示
@@ -35,7 +35,7 @@ export default () => {
         </Card.Header>
         <Card.Body onClick={() => Toast.info('点击了Body区域')}>卡片内容区域</Card.Body>
         <Card.Cover onClick={() => Toast.info('点击了Cover区域')}>
-          <Image src="https://mir-s3-cdn-cf.behance.net/project_modules/max_632/274faa127009547.61390144590a7.png" />
+          <Image src="/demo_view_1.png" />
         </Card.Cover>
       </Card>
     </>
