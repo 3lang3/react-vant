5c86fa1141006bde8cb68180fe343ecb59edd665
3lang <675483520@qq.com>
2022-07-12 10:34:30 +0800

    fix(Picker): correct value type when columns is single

--- packages/react-vant/src/components/form/demo/shouldUpdate.tsx
+++ packages/react-vant/src/components/form/demo/shouldUpdate.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Button, Radio, Input, Form, Picker } from 'react-vant';
+import React, { Fragment } from 'react';
+import { Button, Input, Form, Picker, Selector, Typography } from 'react-vant';
 
 const columns = ['南京', '苏州', '常州', '淮安', '扬州', '南通', '宿迁', '泰州', '无锡'];
 
@@ -23,24 +23,34 @@ export default () => {
         </div>
       }
     >
-      <Form.Item name="type" label="联系方式">
-        <Radio.Group direction="horizontal">
-          <Radio name="mobile">手机号</Radio>
-          <Radio name="address">住址信息</Radio>
-        </Radio.Group>
+      <Form.Item name="type" label="联系方式" initialValue={['mobile']}>
+        <Selector
+          options={[
+            {
+              label: '手机号',
+              value: 'mobile',
+            },
+            {
+              label: '住址信息',
+              value: 'address',
+            },
+          ]}
+          multiple={true}
+        />
       </Form.Item>
       <Form.Item noStyle shouldUpdate={(p, n) => p.type !== n.type}>
         {({ getFieldValue }) => {
-          const type = getFieldValue('type');
-          if (type === 'mobile')
-            return (
-              <Form.Item name="mobile" label="手机号">
+          const type = getFieldValue('type') || [];
+          const content = [];
+          if (type.includes('mobile'))
+            content.push(
+              <Form.Item key="mobile" name="mobile" label="手机号">
                 <Input placeholder="请输入手机号" />
-              </Form.Item>
+              </Form.Item>,
             );
-          if (type === 'address')
-            return (
-              <>
+          if (type.includes('address'))
+            content.push(
+              <Fragment key="address">
                 <Form.Item
                   name="area"
                   label="区域"
@@ -48,15 +58,15 @@ export default () => {
                   onClick={(_, actions) => actions.current?.open()}
                 >
                   <Picker popup columns={columns}>
-                    {(val) => val || '请选择地址'}
+                    {(val) => (val ? <Typography.Text>{val}</Typography.Text> : '请选择地址')}
                   </Picker>
                 </Form.Item>
                 <Form.Item name="area_address" label="详细地址">
                   <Input.TextArea placeholder="请输入详细地址" />
                 </Form.Item>
-              </>
+              </Fragment>,
             );
-          return null;
+          return content;
         }}
       </Form.Item>
     </Form>
