be77cfbee917f793720bfb512fad652e785bca74
lblblong <1162275271@qq.com>
2022-07-13 18:23:41 +0800

    fix: add missing parentheses

--- packages/react-vant/src/components/utils/supports-passive.ts
+++ packages/react-vant/src/components/utils/supports-passive.ts
@@ -1,8 +1,8 @@
-import canUseDom from "./dom/canUseDom"
+import canUseDom from './dom/canUseDom'
 
 export let supportsPassive = false
 
-if (canUseDom) {
+if (canUseDom()) {
   try {
     const opts = {}
     Object.defineProperty(opts, 'passive', {
@@ -12,5 +12,5 @@ if (canUseDom) {
     })
     window.addEventListener('test-passive', null as any, opts)
     // eslint-disable-next-line no-empty
-  } catch (e) { }
+  } catch (e) {}
 }
