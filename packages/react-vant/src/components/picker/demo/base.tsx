29b3de6bee31a2bf25b12169cb9c793377107497
3lang <675483520@qq.com>
2022-07-20 17:51:41 +0800

    chore: 调整 picker 组件文档demo

--- packages/react-vant/src/components/picker/demo/base.tsx
+++ packages/react-vant/src/components/picker/demo/base.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import { Picker, Toast } from 'react-vant';
+import React from 'react'
+import { Picker, Toast } from 'react-vant'
 
-const columns = ['南京', '苏州', '常州', '淮安', '扬州', '南通', '宿迁', '泰州', '无锡'];
+const columns = [
+  { text: '南京', value: 0 },
+  { text: '苏州', value: 1 },
+  { text: '常州', value: 2 },
+  { text: '淮安', value: 3 },
+  { text: '扬州', value: 4 },
+  { text: '南通', value: 5 },
+  { text: '宿迁', value: 6 },
+  { text: '泰州', value: 7 },
+  { text: '无锡', value: 8 },
+]
 
 export default () => {
   return (
     <Picker
-      title="基础使用"
+      title='基础使用'
       columns={columns}
       onChange={(val: string, selectRow, index: number) => {
-        console.log('选中项: ', selectRow);
-        Toast.info(`选中值${val}，索引: ${index}`);
+        console.log('选中项: ', selectRow)
+        Toast.info(`选中值${val}，索引: ${index}`)
       }}
       onCancel={() => Toast.info('点击取消按钮')}
       onConfirm={() => Toast.info('点击确认按钮')}
     />
-  );
-};
+  )
+}
