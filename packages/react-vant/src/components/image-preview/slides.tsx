04079e707aca85e7ac9d5bceb0475d697e2a67e2
3lang <675483520@qq.com>
2022-07-21 23:21:35 +0800

    fix: correct image-preview key repeat (#517)

--- packages/react-vant/src/components/image-preview/slides.tsx
+++ packages/react-vant/src/components/image-preview/slides.tsx
@@ -1,63 +1,69 @@
-import React, { forwardRef, useImperativeHandle, useRef } from 'react';
-import { useDrag } from '@use-gesture/react';
-import { useSpring, animated } from '@react-spring/web';
-import { Slide } from './slide';
-import { bound } from '../utils/bound';
-import { createNamespace, unitToPx } from '../utils';
-import clsx from 'clsx';
+import React, { forwardRef, useImperativeHandle, useRef } from 'react'
+import { useDrag } from '@use-gesture/react'
+import { useSpring, animated } from '@react-spring/web'
+import { Slide } from './slide'
+import { bound } from '../utils/bound'
+import { createNamespace, unitToPx } from '../utils'
+import clsx from 'clsx'
 
 export type SlidesType = {
-  images: string[];
-  onTap: () => void;
-  maxZoom: number;
-  defaultIndex: number;
-  onIndexChange?: (index: number) => void;
-};
+  images: string[]
+  onTap: () => void
+  maxZoom: number
+  defaultIndex: number
+  onIndexChange?: (index: number) => void
+}
 
 export type SlidesRef = {
-  swipeTo: (index: number, immediate?: boolean) => void;
-};
+  swipeTo: (index: number, immediate?: boolean) => void
+}
 
-const [bem] = createNamespace('image-preview');
+const [bem] = createNamespace('image-preview')
 
 export const Slides = forwardRef<SlidesRef, SlidesType>((props, ref) => {
-
-  const slideWidth = window.innerWidth + unitToPx(16);
+  const slideWidth = window.innerWidth + unitToPx(16)
   const [{ x }, api] = useSpring(() => ({
     x: props.defaultIndex * slideWidth,
     config: { tension: 250, clamp: true },
-  }));
+  }))
 
-  const count = props.images.length;
+  const count = props.images.length
 
   function swipeTo(index: number, immediate = false) {
-    const i = bound(index, 0, count - 1);
-    props.onIndexChange?.(i);
+    const i = bound(index, 0, count - 1)
+    props.onIndexChange?.(i)
     api.start({
       x: i * slideWidth,
       immediate,
-    });
+    })
   }
 
   useImperativeHandle(ref, () => ({
     swipeTo,
-  }));
+  }))
 
-  const dragLockRef = useRef(false);
+  const dragLockRef = useRef(false)
   const bind = useDrag(
-    (state) => {
-      if (dragLockRef.current) return;
-      const [offsetX] = state.offset;
+    state => {
+      if (dragLockRef.current) return
+      const [offsetX] = state.offset
       if (state.last) {
-        const minIndex = Math.floor(offsetX / slideWidth);
-        const maxIndex = minIndex + 1;
-        const velocityOffset = Math.min(state.velocity[0] * 2000, slideWidth) * state.direction[0];
-        swipeTo(bound(Math.round((offsetX + velocityOffset) / slideWidth), minIndex, maxIndex));
+        const minIndex = Math.floor(offsetX / slideWidth)
+        const maxIndex = minIndex + 1
+        const velocityOffset =
+          Math.min(state.velocity[0] * 2000, slideWidth) * state.direction[0]
+        swipeTo(
+          bound(
+            Math.round((offsetX + velocityOffset) / slideWidth),
+            minIndex,
+            maxIndex
+          )
+        )
       } else {
         api.start({
           x: offsetX,
           immediate: true,
-        });
+        })
       }
     },
     {
@@ -67,29 +73,32 @@ export const Slides = forwardRef<SlidesRef, SlidesType>((props, ref) => {
         return {
           left: 0,
           right: (count - 1) * slideWidth,
-        };
+        }
       },
       rubberband: true,
       axis: 'x',
       pointer: { touch: true },
-    },
-  );
+    }
+  )
 
   return (
     <div className={clsx(bem('slides'))} {...bind()}>
-      <animated.div className={clsx(bem('slides-inner'))} style={{ x: x.to((x) => -x) }}>
-        {props.images.map((image) => (
+      <animated.div
+        className={clsx(bem('slides-inner'))}
+        style={{ x: x.to(x => -x) }}
+      >
+        {props.images.map((image, idx) => (
           <Slide
-            key={image}
+            key={`${image}${idx}`}
             image={image}
             onTap={props.onTap}
             maxZoom={props.maxZoom}
-            onZoomChange={(zoom) => {
+            onZoomChange={zoom => {
               if (zoom !== 1) {
-                const index: number = Math.round(x.get() / slideWidth);
+                const index: number = Math.round(x.get() / slideWidth)
                 api.start({
                   x: index * slideWidth,
-                });
+                })
               }
             }}
             dragLockRef={dragLockRef}
@@ -97,5 +106,5 @@ export const Slides = forwardRef<SlidesRef, SlidesType>((props, ref) => {
         ))}
       </animated.div>
     </div>
-  );
-});
+  )
+})
