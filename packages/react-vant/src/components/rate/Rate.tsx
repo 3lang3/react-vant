4cd0c9bad9abb6f7a63e93453f9e69f8aef71b3d
3lang <675483520@qq.com>
2022-07-29 17:22:54 +0800

    fix: 修复 Rate 半星样式错乱问题

--- packages/react-vant/src/components/rate/Rate.tsx
+++ packages/react-vant/src/components/rate/Rate.tsx
@@ -1,151 +1,167 @@
 /* eslint-disable no-nested-ternary */
-import React, { useRef, useMemo } from 'react';
-import { Star, StarO } from '@react-vant/icons';
-import clsx from 'clsx';
-import { RateProps } from './PropsType';
-import { addUnit, createNamespace, preventDefault } from '../utils';
-import useRefs from '../hooks/use-refs';
-import useMergedState from '../hooks/use-merged-state';
-import useEventListener from '../hooks/use-event-listener';
-import { useTouch } from '../hooks';
+import React, { useRef, useMemo } from 'react'
+import { Star, StarO } from '@react-vant/icons'
+import clsx from 'clsx'
+import { RateProps } from './PropsType'
+import { addUnit, createNamespace, preventDefault } from '../utils'
+import useRefs from '../hooks/use-refs'
+import useMergedState from '../hooks/use-merged-state'
+import useEventListener from '../hooks/use-event-listener'
+import { useTouch } from '../hooks'
 
-type RateStatus = 'full' | 'half' | 'void';
+type RateStatus = 'full' | 'half' | 'void'
 
 type RateListItem = {
-  value: number;
-  status: RateStatus;
-};
+  value: number
+  status: RateStatus
+}
 
 function getRateStatus(
   value: number,
   index: number,
   allowHalf: boolean,
-  readOnly: boolean,
+  readOnly: boolean
 ): RateListItem {
   if (value >= index) {
-    return { status: 'full', value: 1 };
+    return { status: 'full', value: 1 }
   }
 
   if (value + 0.5 >= index && allowHalf && !readOnly) {
-    return { status: 'half', value: 0.5 };
+    return { status: 'half', value: 0.5 }
   }
 
   if (value + 1 >= index && allowHalf && readOnly) {
-    const cardinal = 10 ** 10;
+    const cardinal = 10 ** 10
     return {
       status: 'half',
       value: Math.round((value - index + 1) * cardinal) / cardinal,
-    };
+    }
   }
 
-  return { status: 'void', value: 0 };
+  return { status: 'void', value: 0 }
 }
 
-const [bem] = createNamespace('rate');
+const [bem] = createNamespace('rate')
 
-const Rate: React.FC<RateProps> = ({ count, touchable, onChange, ...props }) => {
+const Rate: React.FC<RateProps> = ({
+  count,
+  touchable,
+  onChange,
+  ...props
+}) => {
   const [value, setValue] = useMergedState({
     value: props.value,
     defaultValue: props.defaultValue,
-  });
-  const root = useRef<HTMLDivElement>(null);
-  const touch = useTouch();
-  const [itemRefs, setItemRefs] = useRefs();
+  })
+  const root = useRef<HTMLDivElement>(null)
+  const touch = useTouch()
+  const [itemRefs, setItemRefs] = useRefs()
 
-  const untouchable = () => props.readOnly || props.disabled || !touchable;
+  const untouchable = () => props.readOnly || props.disabled || !touchable
 
   const list = useMemo<RateListItem[]>(
     () =>
       Array(count)
         .fill('')
-        .map((_, i) => getRateStatus(value, i + 1, props.allowHalf, props.readOnly)),
-    [count, value, props.allowHalf, props.readOnly],
-  );
+        .map((_, i) =>
+          getRateStatus(value, i + 1, props.allowHalf, props.readOnly)
+        ),
+    [count, value, props.allowHalf, props.readOnly]
+  )
 
-  const ranges = useRef<{ left: number; score: number }[]>();
+  const ranges = useRef<{ left: number; score: number }[]>()
 
   const updateRanges = () => {
-    const rects = itemRefs.map((item) => item.getBoundingClientRect());
+    const rects = itemRefs.map(item => item.getBoundingClientRect())
 
-    ranges.current = [];
+    ranges.current = []
     rects.forEach((rect, index) => {
       if (props.allowHalf) {
         ranges.current.push(
           { score: index + 0.5, left: rect.left },
-          { score: index + 1, left: rect.left + rect.width / 2 },
-        );
+          { score: index + 1, left: rect.left + rect.width / 2 }
+        )
       } else {
-        ranges.current.push({ score: index + 1, left: rect.left });
+        ranges.current.push({ score: index + 1, left: rect.left })
       }
-    });
-  };
+    })
+  }
 
   const getScoreByPosition = (x: number) => {
     // eslint-disable-next-line no-plusplus
     for (let i = ranges.current.length - 1; i > 0; i--) {
       if (x > ranges.current[i].left) {
-        return ranges.current[i].score;
+        return ranges.current[i].score
       }
     }
-    return props.allowHalf ? 0.5 : 1;
-  };
+    return props.allowHalf ? 0.5 : 1
+  }
 
   const select = (index: number) => {
     if (!props.disabled && !props.readOnly && index !== value) {
-      setValue(index);
-      onChange?.(index);
+      setValue(index)
+      onChange?.(index)
     }
-  };
+  }
 
   const onTouchStart = (event: React.TouchEvent) => {
     if (untouchable()) {
-      return;
+      return
     }
 
-    touch.start(event.nativeEvent);
-    updateRanges();
-  };
+    touch.start(event.nativeEvent)
+    updateRanges()
+  }
 
-  const onTouchMove = (event) => {
+  const onTouchMove = event => {
     if (untouchable()) {
-      return;
+      return
     }
 
-    touch.move(event);
+    touch.move(event)
 
     if (touch.isHorizontal()) {
-      const { clientX } = event.touches[0];
-      preventDefault(event);
-      select(getScoreByPosition(clientX));
+      const { clientX } = event.touches[0]
+      preventDefault(event)
+      select(getScoreByPosition(clientX))
     }
-  };
+  }
 
   const renderStar = (item: RateListItem, index: number) => {
-    const { icon, size, color, gutter, voidIcon, disabled, voidColor, allowHalf, disabledColor } =
-      props;
-    const score = index + 1;
-    const isFull = item.status === 'full';
-    const isVoid = item.status === 'void';
+    const {
+      icon,
+      size,
+      color,
+      gutter,
+      voidIcon,
+      disabled,
+      voidColor,
+      allowHalf,
+      disabledColor,
+    } = props
+    const score = index + 1
+    const isFull = item.status === 'full'
+    const isVoid = item.status === 'void'
 
-    const renderHalf = allowHalf && item.value > 0 && item.value < 1;
+    const renderHalf = allowHalf && item.value > 0 && item.value < 1
 
-    let style;
+    let style
     if (gutter && score !== +count) {
       style = {
-        paddingRight: addUnit(gutter),
-      };
+        marginRight: addUnit(gutter),
+      }
     }
 
     const onClickItem = (event: React.MouseEvent) => {
-      updateRanges();
-      select(allowHalf ? getScoreByPosition(event.clientX) : score);
-    };
+      updateRanges()
+      select(allowHalf ? getScoreByPosition(event.clientX) : score)
+    }
 
     return (
       <div
         key={index}
         ref={setItemRefs(index)}
-        role="radio"
+        role='radio'
         style={style}
         className={clsx(bem('item'))}
         tabIndex={0}
@@ -162,42 +178,48 @@ const Rate: React.FC<RateProps> = ({ count, touchable, onChange, ...props }) => 
           },
         })}
         {renderHalf && (
-          <div className={clsx(bem('icon', ['half']))} style={{ width: `${item.value}em` }}>
-            {React.cloneElement((isVoid ? voidIcon : icon) as React.ReactElement, {
-              className: clsx(bem('icon', [{ disabled, full: !isVoid }])),
-              style: {
-                color: disabled ? disabledColor : isVoid ? voidColor : color,
-                fontSize: size,
-              },
-            })}
+          <div
+            className={clsx(bem('icon', ['half']))}
+            style={{ width: `${item.value * 100}%` }}
+          >
+            {React.cloneElement(
+              (isVoid ? voidIcon : icon) as React.ReactElement,
+              {
+                className: clsx(bem('icon', [{ disabled, full: !isVoid }])),
+                style: {
+                  color: disabled ? disabledColor : isVoid ? voidColor : color,
+                  fontSize: size,
+                },
+              }
+            )}
           </div>
         )}
       </div>
-    );
-  };
+    )
+  }
 
   useEventListener('touchmove', onTouchMove as EventListener, {
     target: root.current,
     depends: [touch.deltaY.current],
-  });
+  })
 
   return (
     <div
       ref={root}
-      role="radiogroup"
+      role='radiogroup'
       className={clsx(
         bem({
           readOnly: props.readOnly,
           disabled: props.disabled,
-        }),
+        })
       )}
       tabIndex={0}
       onTouchStart={onTouchStart}
     >
       {list.map(renderStar)}
     </div>
-  );
-};
+  )
+}
 
 Rate.defaultProps = {
   size: 20,
@@ -206,6 +228,6 @@ Rate.defaultProps = {
   icon: <Star />,
   voidIcon: <StarO />,
   touchable: true,
-};
+}
 
-export default Rate;
+export default Rate
