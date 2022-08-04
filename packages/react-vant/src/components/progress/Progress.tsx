d3080d78c25925322b29890ebef3e8679f373f76
3lang <675483520@qq.com>
2022-08-04 21:17:47 +0800

    fix: 修复 `Progress` style 和 className 丢失问题

--- packages/react-vant/src/components/progress/Progress.tsx
+++ packages/react-vant/src/components/progress/Progress.tsx
@@ -1,19 +1,19 @@
-import React, { useMemo } from 'react';
-import clsx from 'clsx';
-import { ProgressProps } from './PropsType';
-import { addUnit, createNamespace } from '../utils';
+import React, { useMemo } from 'react'
+import clsx from 'clsx'
+import { ProgressProps } from './PropsType'
+import { addUnit, createNamespace } from '../utils'
 
-const [bem] = createNamespace('progress');
+const [bem] = createNamespace('progress')
 
-const Progress: React.FC<ProgressProps> = (props) => {
+const Progress: React.FC<ProgressProps> = props => {
   const background = useMemo(
     () => (props.inactive ? '#cacaca' : props.color),
-    [props.inactive, props.color],
-  );
+    [props.inactive, props.color]
+  )
 
   const renderPivot = () => {
-    const { textColor, pivotText, pivotColor, percentage } = props;
-    const text = pivotText ?? `${percentage}%`;
+    const { textColor, pivotText, pivotColor, percentage } = props
+    const text = pivotText ?? `${percentage}%`
 
     if (props.showPivot && text) {
       const style = {
@@ -21,38 +21,39 @@ const Progress: React.FC<ProgressProps> = (props) => {
         left: `${+percentage}%`,
         transform: `translate(-${+percentage}%,-50%)`,
         background: pivotColor || background,
-      };
+      }
 
       return (
         <span style={style} className={clsx(bem('pivot'))}>
           {text}
         </span>
-      );
+      )
     }
-    return null;
-  };
+    return null
+  }
 
-  const { trackColor, percentage, strokeWidth } = props;
+  const { trackColor, percentage, strokeWidth } = props
   const rootStyle = {
+    ...props.style,
     background: trackColor,
     height: addUnit(strokeWidth),
-  };
+  }
   const portionStyle = {
     background,
     transform: `scaleX(${+percentage / 100})`,
-  };
+  }
 
   return (
-    <div className={clsx(bem())} style={rootStyle}>
+    <div className={clsx(bem(), props.className)} style={rootStyle}>
       <span className={clsx(bem('portion'))} style={portionStyle} />
       {renderPivot()}
     </div>
-  );
-};
+  )
+}
 
 Progress.defaultProps = {
   showPivot: true,
   percentage: 0,
-};
+}
 
-export default Progress;
+export default Progress
