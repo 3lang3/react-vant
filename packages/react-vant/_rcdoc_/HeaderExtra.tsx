a2afe6119c0dc9289ef84213318c39d7ac42c4bd
3lang <675483520@qq.com>
2022-08-05 09:43:24 +0800

    chore: 优化文档

--- packages/react-vant/_rcdoc_/HeaderExtra.tsx
+++ packages/react-vant/_rcdoc_/HeaderExtra.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <section className='rv-site-header__extra'>
       <section className='rv-site-header__extra__content'>
-        👀 当前为 <strong>v3</strong> 文档，从低版本
+        🔥 当前为 <strong>v3</strong> 文档，从低版本
         <a href='/guide/migrate'>升级</a> 🚀
       </section>
     </section>
