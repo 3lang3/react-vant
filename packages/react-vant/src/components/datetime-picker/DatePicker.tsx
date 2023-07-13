6dd02a344993a746b0210eca84756df363abbd50
Nick <nickey91.lean@gmail.com>
2023-07-13 11:35:48 +0800

    fix(datetime-picker): datetime-picker 没有值时，弹出层默认选中当前日期/时间。

--- packages/react-vant/src/components/datetime-picker/DatePicker.tsx
+++ packages/react-vant/src/components/datetime-picker/DatePicker.tsx
@@ -37,7 +37,7 @@ const DatePicker = forwardRef<DateTimePickerInstance, DatePickerProps>(
 
     const formatValue = date => {
       if (!isDate(date)) {
-        date = minDate
+        date = new Date()
       }
 
       date = Math.max(date, minDate.getTime())
